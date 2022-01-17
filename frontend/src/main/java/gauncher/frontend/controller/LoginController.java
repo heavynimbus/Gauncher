ae926c3e19a9e6025585d9dd8d2b15a8beaa42cd
llamorille <lamorilleleo@gmail.com>
2022-01-17 23:28:50 +0100

    ✨ : login view implemented

--- frontend/src/main/java/gauncher/frontend/controller/LoginController.java
+++ frontend/src/main/java/gauncher/frontend/controller/LoginController.java
@@ -8,90 +8,80 @@ import gauncher.frontend.logging.Logger;
 import gauncher.frontend.util.TextParser;
 import gauncher.frontend.view.ChatView;
 import gauncher.frontend.view.ConnectionView;
+
 import java.io.IOException;
 import java.net.SocketException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import gauncher.frontend.view.LauncherView;
+import gauncher.frontend.view.SignInView;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Pane;
 
 public class LoginController implements Initializable {
 
-  private Logger log = new Logger("LoginController");
+    private Logger log = new Logger("LoginController");
 
 //  @FXML public Button continueButton;
 //
 //  @FXML public TextField pseudo;
 //  public SimpleStringProperty pseudoValue;
 
-  @FXML
-  private Button connectButton;
+    @FXML
+    private Pane errorMessage;
 
-  @FXML
-  private Button createButton;
+    @FXML
+    private PasswordField passwordInput;
 
-  @FXML
-  private TextField passwordInput;
+    @FXML
+    private TextField userInput;
 
-  @FXML
-  private TextField userInput;
-
-  @Override
-  public void initialize(URL url, ResourceBundle resourceBundle) {
-
-  }
-
-  @FXML
-  public void signIn() {
-
-  }
-
-
-  @FXML
-  public void inputValue(KeyEvent event) {
-    /*TextParser.getDefaultInstance(pseudoValue, pseudo)
-            .addAction(
-                    (e, stringProperty) -> {
-                      if (e.getCode().equals(KeyCode.ENTER)) continueButton.requestFocus();
-                    })
-            .inputValue(event);*/
-  }
-
-  @FXML
-  public void login() throws UnprocessableViewException {
-    /*var pseudoValue = this.pseudoValue.get();
-    if (pseudoValue == null || pseudoValue.isEmpty() || pseudoValue.isBlank()) {
-      pseudo.setStyle("-fx-border-color: red;");
-      pseudo.setOnKeyPressed(
-          (e) -> {
-            pseudo.setStyle("");
-            this.inputValue(e);
-            pseudo.setOnKeyPressed(this::inputValue);
-          });
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        errorMessage.setVisible(false);
     }
-    try {
-      App.client.println(format("LOGIN %s", pseudoValue));
-      var response = App.client.readLine();
-      log.info(format("%s is not already used", pseudoValue));
-      if (response.startsWith("OK")) {
-        App.client.println("USERNAME");
-        response = App.client.readLine();
-        App.client.setPseudo(response);
-        App.setCurrentScene(new LauncherView());
-      }
-    } catch (SocketException e) {
-      log.error("Client have been disconnected from the server");
-      App.setCurrentScene(new ConnectionView());
-    } catch (IOException e) {
-      e.printStackTrace();
+
+    @FXML
+    public void signIn() throws UnprocessableViewException {
+        App.setCurrentScene(new SignInView());
     }
-  }*/
-  }
+
+    @FXML
+    public void login() throws UnprocessableViewException {
+
+        var pseudoValue = this.userInput.getCharacters().toString();
+        var passwordValue = this.passwordInput.getCharacters().toString();
+
+        try {
+            if (pseudoValue.isEmpty() || pseudoValue.isBlank()
+                    || passwordValue.isEmpty() || passwordValue.isBlank()) {
+                App.client.println("LOGIN");
+                this.errorMessage.setVisible(true);
+            } else {
+                App.client.println(format("LOGIN %s %s", pseudoValue, passwordValue));
+            }
+            var response = App.client.readLine();
+            System.out.println(response);
+            if (response.startsWith("OK")) {
+                App.client.setPseudo(pseudoValue);
+                App.setCurrentScene(new LauncherView());
+            } else {
+                this.errorMessage.setVisible(true);
+            }
+
+        } catch (SocketException e) {
+            log.error("Client have been disconnected from the server");
+            App.setCurrentScene(new ConnectionView());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
