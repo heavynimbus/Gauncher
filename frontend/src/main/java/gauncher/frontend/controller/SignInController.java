adc1e28448ed28552cf102144aa800c263eeca1e
llamorille <lamorilleleo@gmail.com>
2022-01-17 23:38:26 +0100

    🐛 : fix du bug d'affichage de la sign in view

--- frontend/src/main/java/gauncher/frontend/controller/SignInController.java
+++ frontend/src/main/java/gauncher/frontend/controller/SignInController.java
@@ -1,13 +1,32 @@
 package gauncher.frontend.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SignInController implements Initializable {
 
+    @FXML
+    private Text confirmErrorPass;
 
+    @FXML
+    private Text messErrorPseudo;
+
+    @FXML
+    private TextField password;
+
+    @FXML
+    private TextField pseudo;
+
+    @FXML
+    void signIn(ActionEvent event) {
+
+    }
 
 
     @Override
