e0e75cd6cb44170c472253a5e1d99345c0555b89
llamorille <lamorilleleo@gmail.com>
2022-01-18 18:23:25 +0100

    🚧 : work in progress (affichage des images)

--- frontend/src/main/java/gauncher/frontend/controller/LauncherController.java
+++ frontend/src/main/java/gauncher/frontend/controller/LauncherController.java
@@ -12,13 +12,15 @@ import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-import java.io.InputStream;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class LauncherController implements Initializable {
 
-    Class aClass = this.getClass();
+    public final static String LOGO_TICTACTOE = "@/img/logoLogout.png.png";
+
+    Class<?> aClass = this.getClass();
 
     @FXML
     private ImageView buttonBattleQuiz;
@@ -52,7 +54,19 @@ public class LauncherController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        System.out.println(App.client.getPseudo().get());
+//        InputStream inputTicTacToe =  aClass.getResourceAsStream(LOGO_TICTACTOE);
+//        assert inputTicTacToe != null;
+//        Image imageTicTacToe = new Image(Objects.requireNonNull(getClass().getResource("img/logoLogout.png")).toExternalForm());
+//        buttonMorpion.setVisible(true);
+        var img = new Image(Objects.requireNonNull(getClass().getResource("/gauncher/frontend/controller/logoLogout.png")).toString(), true);
+//        buttonMorpion.setImage(new Image("/home/llamorille/Documents/ISEN-Lille/Projets/Gauncher/frontend/src/main/resources/img/logoMorpion.png"));
+//        var scene = buttonMorpion.getScene();
+//        var img = new Image("/frontend/src/main/resources/img/logoMorpion.png");
+//        var img = new Image(inputTicTacToe);
+//        buttonMorpion.setVisible(true);
+//        System.out.println("buttonMorpion = " + buttonMorpion.getImage());
+//        System.out.println("scene = " + scene);0
+        buttonMorpion.setImage(img);
         pseudoLabel.setText(App.client.getPseudo().get());
     }
 
--- frontend/src/main/java/gauncher/frontend/controller/SignInController.java
+++ frontend/src/main/java/gauncher/frontend/controller/SignInController.java
@@ -1,36 +1,84 @@
 package gauncher.frontend.controller;
 
+import static java.lang.String.format;
+
+import gauncher.frontend.App;
+import gauncher.frontend.exception.UnprocessableViewException;
+import gauncher.frontend.logging.Logger;
+import gauncher.frontend.view.ConnectionView;
+import gauncher.frontend.view.LauncherView;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import java.io.IOException;
+import java.net.SocketException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class SignInController implements Initializable {
 
+    private Logger log = new Logger("SignInController");
+
     @FXML
     private Text confirmErrorPass;
+
+    @FXML
+    private PasswordField confirmPassword;
 
     @FXML
     private Text messErrorPseudo;
 
     @FXML
-    private TextField password;
+    private PasswordField password;
 
     @FXML
     private TextField pseudo;
 
     @FXML
-    void signIn(ActionEvent event) {
+    void signIn(ActionEvent event) throws UnprocessableViewException {
+        var pseudoValue = this.pseudo.getCharacters().toString();
+        var passwordValue = this.password.getCharacters().toString();
+        var confirmPasswordValue = this.confirmPassword.getCharacters().toString();
 
+        try {
+            if (pseudoValue.isBlank() || pseudoValue.isEmpty()
+                    || passwordValue.isBlank() || passwordValue.isEmpty()
+                    || confirmPasswordValue.isEmpty() || confirmPasswordValue.isBlank()
+                    || !passwordValue.equals(confirmPasswordValue)) {
+                if (pseudoValue.isEmpty()) {
+                    this.messErrorPseudo.setVisible(true);
+                }
+                if(!passwordValue.equals(confirmPasswordValue)) {
+                    this.confirmErrorPass.setVisible(true);
+                }
+            } else {
+                App.client.println(format("SIGN %s %s", pseudoValue, passwordValue));
+                var response = App.client.readLine();
+                if (response.startsWith("OK")) {
+                    App.client.setPseudo(pseudoValue);
+                    App.setCurrentScene(new LauncherView());
+                }
+            }
+        } catch (SocketException e) {
+            log.error("Client have been disconnected from the server");
+            App.setCurrentScene(new ConnectionView());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        pseudo.setText("");
+        password.setText("");
+        confirmPassword.setText("");
+        messErrorPseudo.setVisible(false);
+        confirmErrorPass.setVisible(false);
+        log.info("Sign In initialized");
     }
 }
